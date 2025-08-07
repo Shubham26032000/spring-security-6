2bbb56654554ef195374183d63baf86d3888d7d9
Shubham <shubhamvast5@gmail.com>
2025-08-07 18:13:27 +0530

    Implemented custom users which can login to our system using UserDetailsService.

---
+++ spring-security-6/src/main/java/com/shubham/security/config/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.shubham.security.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests( request -> request.anyRequest().authenticated()) //authenticate request
+//                .csrf(Customizer.withDefaults())      //CSRF with default configuration /by default present
+                .csrf(csrf -> csrf.disable())  //to disable the csrf.
+                .formLogin(Customizer.withDefaults()) //Add form based login
+                .httpBasic(Customizer.withDefaults()); //have basic authetication
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+         UserDetails shubham = User.withUsername("shubham").password("{noop}123").roles("USER").build();
+        UserDetails pranit = User.withUsername("pranit").password("{noop}123").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(shubham,pranit);
+    }
+}
+
